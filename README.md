6cdce86d9970085c14bfe1d316c4d5f8780c4f4b
liaozhenting <liaozhenting>
2017-07-05 17:20:24 +0800

    +gitignore

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 # Vue-blog
 Vue后台博客管理界面。
 
+### 使用
+安装包
+```
+npm install
+
+```
+开启服务
+```
+npm start
+```
--- src/components/acount.vue
+++ src/components/acount.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div class="btn btn-default"></div>
     <table v-for='item in items'>
       <tr>
         <tr><td>账号名：</td><td><input type="text" v-model='item.username'/></td></tr>
@@ -45,22 +46,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 
-a {
-  color: #42b983;
-}
-</style>
